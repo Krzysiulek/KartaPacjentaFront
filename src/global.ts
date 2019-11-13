985aae77b5e4023b23802cbb4283e4f332617ed1
Krzysztof Czarnecki <czarneckikrzysztof97@gmail.com>
2019-11-13 09:58:08 +0100

    fixed login to service

--- src/global.ts
+++ src/global.ts
@@ -1,4 +1,4 @@
 'use strict';
 
 // export const serviceUrl: string = 'http://localhost:8080';
-export const serviceUrl: string="https://trunk-kartapacjentaservice.herokuapp.com/";
+export const serviceUrl: string="https://trunk-kartapacjentaservice.herokuapp.com";
