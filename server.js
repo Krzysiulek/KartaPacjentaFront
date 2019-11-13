5167104158515ad5047c6ad10a1c6dfbbf2bb5a2
Krzysztof Czarnecki <czarneckikrzysztof97@gmail.com>
2019-11-13 06:41:38 +0100

    prepare to deploy to server

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/karta-pacjenta-front'));
+app.use(express.static(__dirname + '/dist/kartaPacjentaFront'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/karta-pacjenta-front/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/kartaPacjentaFront/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
